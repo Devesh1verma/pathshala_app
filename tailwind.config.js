9bc3686231839c12fe1831cea73d18a394d9fedc
Devesh1verma <desverm@gmail.com>
2024-07-07 20:25:07 +0530

    3rd commit message

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  darkMode: 'class', // or 'media' if you prefer system preference
   theme: {
+    extend: {
+      colors: {
+        'gray-900': '#18191F',
+        'gray-200': '#717171',
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
-}
-
+};
